a97c8613ad9ed6c72e802d4288a94a686580c891
Paolo Compagnoni <compagnonipaolo95@gmail.com>
2022-03-24 22:57:31 +0100

    Correzione dei messaggi diagnostici
    I messaggi diagnostici ora sono più chiari

--- odometry_updater.cpp
+++ odometry_updater.cpp
@@ -60,202 +60,202 @@ void xPosePositionDiagostic(diagnostic_updater::DiagnosticStatusWrapper &stat)
 {
   if (x_pose_position > x_pose_position_threshold)
     
-    stat.summaryf(diagnostic_msgs::DiagnosticStatus::ERROR, "Il valore x ha superato %f", x_pose_position_threshold);
+    stat.summaryf(diagnostic_msgs::DiagnosticStatus::ERROR, "Il valore x_pose_position e' fuori soglia! Valore limite: %f", x_pose_position_threshold);
   else
     
-    stat.summaryf(diagnostic_msgs::DiagnosticStatus::OK, "Va tutto bene x: %f", x_pose_position);
+    stat.summaryf(diagnostic_msgs::DiagnosticStatus::OK, "Il valore rientra nella norma, x_pose_position: %f", x_pose_position);
 
   
-  stat.add("Diagnostica di esempio", "Controllo del valore della x");
+  stat.add("Diagnostica Pose Position", "Valore");
   std::ostringstream x_pose_position_string;
   x_pose_position_string << x_pose_position;
-  stat.addf("Posizione x:", x_pose_position_string.str().c_str());
+  stat.addf("x Pose Position", x_pose_position_string.str().c_str());
 }
 
 void yPosePositionDiagostic(diagnostic_updater::DiagnosticStatusWrapper &stat)
 {
   if (y_pose_position > y_pose_position_threshold)
     
-    stat.summaryf(diagnostic_msgs::DiagnosticStatus::ERROR, "Il valore y ha superato %f", y_pose_position_threshold);
+    stat.summaryf(diagnostic_msgs::DiagnosticStatus::ERROR, "Il valore y_pose_position e' fuori soglia! Valore limite: %f", y_pose_position_threshold);
   else
     
-    stat.summaryf(diagnostic_msgs::DiagnosticStatus::OK, "Va tutto bene y: %f", y_pose_position);
+    stat.summaryf(diagnostic_msgs::DiagnosticStatus::OK, "Il valore rientra nella norma, y_pose_position: %f", y_pose_position);
 
   
-  stat.add("Diagnostica di esempio", "Controllo del valore della y");
+  stat.add("Diagnostica Pose Position", "Valore");
   std::ostringstream y_pose_position_string;
   y_pose_position_string << y_pose_position;
-  stat.addf("Posizione y:", y_pose_position_string.str().c_str());
+  stat.addf("y Pose Position", y_pose_position_string.str().c_str());
 }
 
 void zPosePositionDiagostic(diagnostic_updater::DiagnosticStatusWrapper &stat)
 {
   if (z_pose_position > z_pose_position_threshold)
     
-    stat.summaryf(diagnostic_msgs::DiagnosticStatus::ERROR, "Il valore z ha superato %f", z_pose_position_threshold);
+    stat.summaryf(diagnostic_msgs::DiagnosticStatus::ERROR, "Il valore z_pose_position e' fuori soglia! Valore limite: %f", z_pose_position_threshold);
   else
     
-    stat.summaryf(diagnostic_msgs::DiagnosticStatus::OK, "Va tutto bene z: %f", z_pose_position);
+    stat.summaryf(diagnostic_msgs::DiagnosticStatus::OK, "Il valore rientra nella norma, z_pose_position: %f", z_pose_position);
 
   
-  stat.add("Diagnostica di esempio", "Controllo del valore della z");
+  stat.add("Diagnostica Pose Position", "Valore");
   std::ostringstream z_pose_position_string;
   z_pose_position_string << z_pose_position;
-  stat.addf("Posizione z:", z_pose_position_string.str().c_str());
+  stat.addf("z Pose Position", z_pose_position_string.str().c_str());
 }
 
 void xPoseOrientationDiagostic(diagnostic_updater::DiagnosticStatusWrapper &stat)
 {
   if (x_pose_orientation > x_pose_orientation_threshold)
     
-    stat.summaryf(diagnostic_msgs::DiagnosticStatus::ERROR, "Il valore x ha superato %f", x_pose_orientation_threshold);
+    stat.summaryf(diagnostic_msgs::DiagnosticStatus::ERROR, "Il valore x_pose_orientation e' fuori soglia! Valore limite: %f", x_pose_orientation_threshold);
   else
     
-    stat.summaryf(diagnostic_msgs::DiagnosticStatus::OK, "Va tutto bene x: %f", x_pose_orientation);
+    stat.summaryf(diagnostic_msgs::DiagnosticStatus::OK, "Il valore rientra nella norma, x_pose_orientation: %f", x_pose_orientation);
 
   
-  stat.add("Diagnostica di esempio", "Controllo del valore della x");
+  stat.add("Diagnostica Pose Orientation", "Valore");
   std::ostringstream x_pose_orientation_string;
   x_pose_orientation_string << x_pose_orientation;
-  stat.addf("Posizione x:", x_pose_orientation_string.str().c_str());
+  stat.addf("x pose orientation", x_pose_orientation_string.str().c_str());
 }
 
 void yPoseOrientationDiagostic(diagnostic_updater::DiagnosticStatusWrapper &stat)
 {
   if (y_pose_orientation > y_pose_orientation_threshold)
     
-    stat.summaryf(diagnostic_msgs::DiagnosticStatus::ERROR, "Il valore y ha superato %f", y_pose_orientation_threshold);
+    stat.summaryf(diagnostic_msgs::DiagnosticStatus::ERROR, "Il valore y_pose_orientation e' fuori soglia! Valore limite: %f", y_pose_orientation_threshold);
   else
     
-    stat.summaryf(diagnostic_msgs::DiagnosticStatus::OK, "Va tutto bene y: %f", y_pose_orientation);
+    stat.summaryf(diagnostic_msgs::DiagnosticStatus::OK, "Il valore rientra nella norma, y_pose_orientation: %f", y_pose_orientation);
 
   
-  stat.add("Diagnostica di esempio", "Controllo del valore della y");
+  stat.add("Diagnostica Pose Orientation", "Valore");
   std::ostringstream y_pose_orientation_string;
   y_pose_orientation_string << y_pose_orientation;
-  stat.addf("Posizione y:", y_pose_orientation_string.str().c_str());
+  stat.addf("y pose orientation", y_pose_orientation_string.str().c_str());
 }
 
 void zPoseOrientationDiagostic(diagnostic_updater::DiagnosticStatusWrapper &stat)
 {
   if (z_pose_orientation > z_pose_orientation_threshold)
   
-    stat.summaryf(diagnostic_msgs::DiagnosticStatus::ERROR, "Il valore z ha superato %f", z_pose_orientation_threshold);
+    stat.summaryf(diagnostic_msgs::DiagnosticStatus::ERROR, "Il valore z_pose_orientation e' fuori soglia! Valore limite: %f", z_pose_orientation_threshold);
   else
     
-    stat.summaryf(diagnostic_msgs::DiagnosticStatus::OK, "Va tutto bene z: %f", z_pose_orientation);
+    stat.summaryf(diagnostic_msgs::DiagnosticStatus::OK, "Il valore rientra nella norma, z_pose_orientation: %f", z_pose_orientation);
 
   
-  stat.add("Diagnostica di esempio", "Controllo del valore della z");
+  stat.add("Diagnostica Pose Orientation", "Valore");
   std::ostringstream z_pose_orientation_string;
   z_pose_orientation_string << z_pose_orientation;
-  stat.addf("Posizione z:", z_pose_orientation_string.str().c_str());
+  stat.addf("z pose orientation", z_pose_orientation_string.str().c_str());
 }
 
 void wPoseOrientationDiagostic(diagnostic_updater::DiagnosticStatusWrapper &stat)
 {
   if (w_pose_orientation > w_pose_orientation_threshold)
   
-    stat.summaryf(diagnostic_msgs::DiagnosticStatus::ERROR, "Il valore w ha superato %f", w_pose_orientation_threshold);
+    stat.summaryf(diagnostic_msgs::DiagnosticStatus::ERROR, "Il valore w_pose_orientation e' fuori soglia! Valore limite: %f", w_pose_orientation_threshold);
   else
     
-    stat.summaryf(diagnostic_msgs::DiagnosticStatus::OK, "Va tutto bene w: %f", w_pose_orientation);
+    stat.summaryf(diagnostic_msgs::DiagnosticStatus::OK, "Il valore rientra nella norma, w_pose_orientation: %f", w_pose_orientation);
 
   
-  stat.add("Diagnostica di esempio", "Controllo del valore della w");
+  stat.add("Diagnostica Pose Orientation", "Valore");
   std::ostringstream w_pose_orientation_string;
   w_pose_orientation_string << w_pose_orientation;
-  stat.addf("Posizione w:", w_pose_orientation_string.str().c_str());
+  stat.addf("w pose orientation", w_pose_orientation_string.str().c_str());
 }
 
 void xTwistLinearDiagnostic(diagnostic_updater::DiagnosticStatusWrapper &stat)
 {
   if (x_twist_linear > x_twist_linear_threshold)
   
-    stat.summaryf(diagnostic_msgs::DiagnosticStatus::ERROR, "Il valore x ha superato %f", x_twist_linear_threshold);
+    stat.summaryf(diagnostic_msgs::DiagnosticStatus::ERROR, "Il valore x_twist_linear e' fuori soglia! Valore limite: %f", x_twist_linear_threshold);
   else
     
-    stat.summaryf(diagnostic_msgs::DiagnosticStatus::OK, "Va tutto bene x: %f", x_twist_linear_threshold);
+    stat.summaryf(diagnostic_msgs::DiagnosticStatus::OK, "Il valore rientra nella norma, x_twist_linear: %f", x_twist_linear);
   
-  stat.add("Diagnostica di esempio", "Controllo del valore della x");
+  stat.add("Diagnostica Twist Linear", "Valore");
   std::ostringstream x_twist_linear_string;
   x_twist_linear_string << x_twist_linear;
-  stat.addf("Posizione x:", x_twist_linear_string.str().c_str());
+  stat.addf("x Twist Linear", x_twist_linear_string.str().c_str());
 }
 
 void yTwistLinearDiagnostic(diagnostic_updater::DiagnosticStatusWrapper &stat)
 {
   if (y_twist_linear > y_twist_linear_threshold)
   
-    stat.summaryf(diagnostic_msgs::DiagnosticStatus::ERROR, "Il valore y ha superato %f", y_twist_linear_threshold);
+    stat.summaryf(diagnostic_msgs::DiagnosticStatus::ERROR, "Il valore y_twist_linear e' fuori soglia! Valore limite: %f", y_twist_linear_threshold);
   else
     
-    stat.summaryf(diagnostic_msgs::DiagnosticStatus::OK, "Va tutto bene y: %f", y_twist_linear_threshold);
+    stat.summaryf(diagnostic_msgs::DiagnosticStatus::OK, "Il valore rientra nella norma, y_twist_linear: %f", y_twist_linear);
   
-  stat.add("Diagnostica di esempio", "Controllo del valore della y");
+  stat.add("Diagnostica Twist Linear", "Valore");
   std::ostringstream y_twist_linear_string;
   y_twist_linear_string << y_twist_linear;
-  stat.addf("Posizione y:", y_twist_linear_string.str().c_str());
+  stat.addf("y Twist Linear", y_twist_linear_string.str().c_str());
 }
 
 void zTwistLinearDiagnostic(diagnostic_updater::DiagnosticStatusWrapper &stat)
 {
   if (z_twist_linear > z_twist_linear_threshold)
   
-    stat.summaryf(diagnostic_msgs::DiagnosticStatus::ERROR, "Il valore z ha superato %f", z_twist_linear_threshold);
+    stat.summaryf(diagnostic_msgs::DiagnosticStatus::ERROR, "Il valore z_twist_linear e' fuori soglia! Valore limite: %f", z_twist_linear_threshold);
   else
     
-    stat.summaryf(diagnostic_msgs::DiagnosticStatus::OK, "Va tutto bene z: %f", z_twist_linear_threshold);
+    stat.summaryf(diagnostic_msgs::DiagnosticStatus::OK, "Il valore rientra nella norma, z_twist_linear: %f", z_twist_linear);
   
-  stat.add("Diagnostica di esempio", "Controllo del valore della z");
+  stat.add("Diagnostica Twist Linear", "Valore");
   std::ostringstream z_twist_linear_string;
   z_twist_linear_string << z_twist_linear;
-  stat.addf("Posizione z:", z_twist_linear_string.str().c_str());
+  stat.addf("z Twist Linear", z_twist_linear_string.str().c_str());
 }
 
 void xTwistAngularDiagnostic(diagnostic_updater::DiagnosticStatusWrapper &stat)
 {
   if (x_twist_angular > x_twist_angular_threshold)
   
-    stat.summaryf(diagnostic_msgs::DiagnosticStatus::ERROR, "Il valore x ha superato %f", x_twist_angular_threshold);
+    stat.summaryf(diagnostic_msgs::DiagnosticStatus::ERROR, "Il valore x_twist_angular e' fuori soglia! Valore limite: %f", x_twist_angular_threshold);
   else
     
-    stat.summaryf(diagnostic_msgs::DiagnosticStatus::OK, "Va tutto bene x: %f", x_twist_angular_threshold);
+    stat.summaryf(diagnostic_msgs::DiagnosticStatus::OK, "Il valore rientra nella norma, x_twist_angular: %f", x_twist_angular);
   
-  stat.add("Diagnostica di esempio", "Controllo del valore della x");
+  stat.add("Diagnostica Twist Angular", "Valore");
   std::ostringstream x_twist_angular_string;
   x_twist_angular_string << x_twist_angular;
-  stat.addf("Posizione x:", x_twist_angular_string.str().c_str());
+  stat.addf("x Twist Angular", x_twist_angular_string.str().c_str());
 }
 
 void yTwistAngularDiagnostic(diagnostic_updater::DiagnosticStatusWrapper &stat)
 {
   if (y_twist_angular > y_twist_angular_threshold)
   
-    stat.summaryf(diagnostic_msgs::DiagnosticStatus::ERROR, "Il valore y ha superato %f", y_twist_angular_threshold);
+    stat.summaryf(diagnostic_msgs::DiagnosticStatus::ERROR, "Il valore y_twist_angular e' fuori soglia! Valore limite: %f", y_twist_angular_threshold);
   else
     
-    stat.summaryf(diagnostic_msgs::DiagnosticStatus::OK, "Va tutto bene y: %f", y_twist_angular_threshold);
+    stat.summaryf(diagnostic_msgs::DiagnosticStatus::OK, "Il valore rientra nella norma, y_twist_angular: %f", y_twist_angular);
   
-  stat.add("Diagnostica di esempio", "Controllo del valore della y");
+  stat.add("Diagnostica Twist Angular", "Valore");
   std::ostringstream y_twist_angular_string;
   y_twist_angular_string << y_twist_angular;
-  stat.addf("Posizione y:", y_twist_angular_string.str().c_str());
+  stat.addf("y Twist Angular", y_twist_angular_string.str().c_str());
 }
 
 void zTwistAngularDiagnostic(diagnostic_updater::DiagnosticStatusWrapper &stat)
 {
   if (z_twist_angular > z_twist_angular_threshold)
   
-    stat.summaryf(diagnostic_msgs::DiagnosticStatus::ERROR, "Il valore z ha superato %f", z_twist_angular_threshold);
+    stat.summaryf(diagnostic_msgs::DiagnosticStatus::ERROR, "Il valore z_twist_angular e' fuori soglia! Valore limite: %f", z_twist_angular_threshold);
   else
     
-    stat.summaryf(diagnostic_msgs::DiagnosticStatus::OK, "Va tutto bene z: %f", z_twist_angular_threshold);
+    stat.summaryf(diagnostic_msgs::DiagnosticStatus::OK, "Il valore rientra nella norma, z_twist_angular: %f", z_twist_angular);
   
-  stat.add("Diagnostica di esempio", "Controllo del valore della z");
+  stat.add("Diagnostica Twist Angular", "Valore");
   std::ostringstream z_twist_angular_string;
   z_twist_angular_string << z_twist_angular;
-  stat.addf("Posizione z:", z_twist_angular_string.str().c_str());
+  stat.addf("z Twist Angular", z_twist_angular_string.str().c_str());
 }
 
 int main(int argc, char **argv)
