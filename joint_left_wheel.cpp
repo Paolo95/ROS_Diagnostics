4e6788d1001fa69ee9cf0bc5e82821ce8d02cc01
Paolo Compagnoni <compagnonipaolo95@gmail.com>
2022-04-06 13:52:52 +0200

    Aggiornati i wheel joint con nuove variabili
    Aggiunte le variabili:
    -output
    -p_error
    -i_error
    -d_error
    -error
    -error_dot
    
    Modificati i nomi nelle classi cpp per evitare side effects

--- joint_left_wheel.cpp
+++ joint_left_wheel.cpp
@@ -4,29 +4,123 @@
 #include <control_msgs/PidState.h>
 #include <sstream>
 
-float left_wheel_joint_values = 0.0;
-float left_wheel_joint_values_threshold = 0.0;
+float left_output = 0.0;
+float left_output_threshold = 0.0;
+float left_p_error = 0.0;
+float left_p_error_threshold = 0.0;
+float left_i_error = 0.0;
+float left_i_error_threshold = 0.0;
+float left_d_error = 0.0;
+float left_d_error_threshold = 0.0;
+float left_error = 0.0;
+float left_error_threshold = 0.0;
+float left_error_dot = 0.0;
+float left_error_dot_threshold = 0.0;
 
 void LeftJointCallback(const control_msgs::PidState::ConstPtr& msg)
 {
-  
-    left_wheel_joint_values = msg->output;
+  left_output = msg->output;
+  left_p_error = msg->p_error;
+  left_i_error = msg->i_error;
+  left_d_error = msg->d_error;
+  left_error = msg->error;
+  left_error_dot = msg->error_dot;
 }
 
-void LeftJointDiagostic(diagnostic_updater::DiagnosticStatusWrapper &stat)
+void LeftJointOutputDiagostic(diagnostic_updater::DiagnosticStatusWrapper &stat)
 {
-  if (left_wheel_joint_values > left_wheel_joint_values_threshold)
+  if (left_output > left_output_threshold)
     
-    stat.summaryf(diagnostic_msgs::DiagnosticStatus::ERROR, "%f.", left_wheel_joint_values);
+    stat.summaryf(diagnostic_msgs::DiagnosticStatus::ERROR, "Il valore e' fuori soglia! Soglia: %f.", left_output_threshold);
   else
     
-    stat.summaryf(diagnostic_msgs::DiagnosticStatus::OK, "Il valore rientra nella norma, left_wheel_joint_values: %f", left_wheel_joint_values);
+    stat.summaryf(diagnostic_msgs::DiagnosticStatus::OK, "Il valore rientra nella norma, left_output: %f", left_output);
 
   
   stat.add("Diagnostica left_wheel_joint", "Valore");
-  std::ostringstream left_wheel_joint_values_string;
-  left_wheel_joint_values_string << left_wheel_joint_values;
-  stat.addf("Left Wheel output", left_wheel_joint_values_string.str().c_str());
+  std::ostringstream left_output_string;
+  left_output_string << left_output;
+  stat.addf("Left Wheel output", left_output_string.str().c_str());
+}
+
+void LeftJointPErrorDiagostic(diagnostic_updater::DiagnosticStatusWrapper &stat)
+{
+  if (left_p_error > left_p_error_threshold)
+    
+    stat.summaryf(diagnostic_msgs::DiagnosticStatus::ERROR, "Il valore e' fuori soglia! Soglia: %f.", left_p_error_threshold);
+  else
+    
+    stat.summaryf(diagnostic_msgs::DiagnosticStatus::OK, "Il valore rientra nella norma, left_p_error: %f", left_p_error);
+
+  
+  stat.add("Diagnostica left_wheel_joint", "Valore");
+  std::ostringstream left_p_error_string;
+  left_p_error_string << left_p_error;
+  stat.addf("Left Wheel p_error", left_p_error_string.str().c_str());
+}
+
+void LeftJointIErrorDiagostic(diagnostic_updater::DiagnosticStatusWrapper &stat)
+{
+  if (left_i_error > left_i_error_threshold)
+    
+    stat.summaryf(diagnostic_msgs::DiagnosticStatus::ERROR, "Il valore e' fuori soglia! Soglia: %f.", left_i_error_threshold);
+  else
+    
+    stat.summaryf(diagnostic_msgs::DiagnosticStatus::OK, "Il valore rientra nella norma, left_i_error: %f", left_i_error);
+
+  
+  stat.add("Diagnostica left_wheel_joint", "Valore");
+  std::ostringstream left_i_error_string;
+  left_i_error_string << left_i_error;
+  stat.addf("Left Wheel i_error", left_i_error_string.str().c_str());
+}
+
+void LeftJointDErrorDiagostic(diagnostic_updater::DiagnosticStatusWrapper &stat)
+{
+  if (left_d_error > left_d_error_threshold)
+    
+    stat.summaryf(diagnostic_msgs::DiagnosticStatus::ERROR, "Il valore e' fuori soglia! Soglia: %f.", left_d_error_threshold);
+  else
+    
+    stat.summaryf(diagnostic_msgs::DiagnosticStatus::OK, "Il valore rientra nella norma, left_d_error: %f", left_d_error);
+
+  
+  stat.add("Diagnostica left_wheel_joint", "Valore");
+  std::ostringstream left_d_error_string;
+  left_d_error_string << left_d_error;
+  stat.addf("Left Wheel d_error", left_d_error_string.str().c_str());
+}
+
+void LeftJointErrorDiagostic(diagnostic_updater::DiagnosticStatusWrapper &stat)
+{
+  if (left_error > left_error_threshold)
+    
+    stat.summaryf(diagnostic_msgs::DiagnosticStatus::ERROR, "Il valore e' fuori soglia! Soglia: %f.", left_error_threshold);
+  else
+    
+    stat.summaryf(diagnostic_msgs::DiagnosticStatus::OK, "Il valore rientra nella norma, left_error: %f", left_error);
+
+  
+  stat.add("Diagnostica left_wheel_joint", "Valore");
+  std::ostringstream left_error_string;
+  left_error_string << left_error;
+  stat.addf("Left Wheel error", left_error_string.str().c_str());
+}
+
+void LeftJointErrorDotDiagostic(diagnostic_updater::DiagnosticStatusWrapper &stat)
+{
+  if (left_error_dot > left_error_dot_threshold)
+    
+    stat.summaryf(diagnostic_msgs::DiagnosticStatus::ERROR, "Il valore e' fuori soglia! Soglia: %f.", left_error_dot_threshold);
+  else
+    
+    stat.summaryf(diagnostic_msgs::DiagnosticStatus::OK, "Il valore rientra nella norma, left_error_dot: %f", left_error_dot);
+
+  
+  stat.add("Diagnostica left_wheel_joint", "Valore");
+  std::ostringstream left_error_dot_string;
+  left_error_dot_string << left_error_dot;
+  stat.addf("Left Wheel error_dot", left_error_dot_string.str().c_str());
 }
 
 int main(int argc, char **argv)
@@ -34,13 +128,24 @@ int main(int argc, char **argv)
   ros::init(argc, argv, "left_wheel_joint_diagnostic");
   ros::NodeHandle nh;
   diagnostic_updater::Updater updater;
-  updater.setHardwareID("wheel_joint/left");
+  updater.setHardwareID("left_wheel_joint/state");
 
-  nh.getParam("/left_joint_wheel_params/left_joint_wheel_thresholds/left_wheel_joint_values_threshold", left_wheel_joint_values_threshold);
+  nh.getParam("/left_joint_wheel_params/left_joint_wheel_thresholds/output", left_output_threshold);
+  nh.getParam("/left_joint_wheel_params/left_joint_wheel_thresholds/output", left_p_error_threshold);
+  nh.getParam("/left_joint_wheel_params/left_joint_wheel_thresholds/output", left_i_error_threshold);
+  nh.getParam("/left_joint_wheel_params/left_joint_wheel_thresholds/output", left_d_error_threshold);
+  nh.getParam("/left_joint_wheel_params/left_joint_wheel_thresholds/output", left_error_threshold);
+  nh.getParam("/left_joint_wheel_params/left_joint_wheel_thresholds/output", left_error_dot_threshold);
 
   ros::Subscriber sub = nh.subscribe("/gazebo_ros_control/pid_gains/left_wheel_joint/state", 1000, LeftJointCallback);
 
-  updater.add("Diagnostica Joint left wheel: ", LeftJointDiagostic);
+  updater.add("Diagnostica Output Joint left wheel: ", LeftJointOutputDiagostic);
+  updater.add("Diagnostica P Error Joint left wheel: ", LeftJointPErrorDiagostic);
+  updater.add("Diagnostica I Error Joint left wheel: ", LeftJointIErrorDiagostic);
+  updater.add("Diagnostica D Error Joint left wheel: ", LeftJointDErrorDiagostic);
+  updater.add("Diagnostica Error Joint left wheel: ", LeftJointErrorDiagostic);
+  updater.add("Diagnostica Error_dot Joint left wheel: ", LeftJointErrorDotDiagostic);
+
 
   while (nh.ok())
   {
--- joint_right_wheel.cpp
+++ joint_right_wheel.cpp
@@ -4,30 +4,124 @@
 #include <control_msgs/PidState.h>
 #include <sstream>
 
-float right_wheel_joint_values = 0.0;
-float right_wheel_joint_values_threshold = 0.0;
+float right_output = 0.0;
+float right_output_threshold = 0.0;
+float right_p_error = 0.0;
+float right_p_error_threshold = 0.0;
+float right_i_error = 0.0;
+float right_i_error_threshold = 0.0;
+float right_d_error = 0.0;
+float right_d_error_threshold = 0.0;
+float right_error = 0.0;
+float right_error_threshold = 0.0;
+float right_error_dot = 0.0;
+float right_error_dot_threshold = 0.0;
 
 
 void RightJointCallback(const control_msgs::PidState::ConstPtr& msg)
 {
-  
-    right_wheel_joint_values = msg->output;
+  right_output = msg->output;
+  right_p_error = msg->p_error;
+  right_i_error = msg->i_error;
+  right_d_error = msg->d_error;
+  right_error = msg->error;
+  right_error_dot = msg->error_dot;
 }
 
-void RightJointDiagostic(diagnostic_updater::DiagnosticStatusWrapper &stat)
+void RightJointOutputDiagostic(diagnostic_updater::DiagnosticStatusWrapper &stat)
 {
-  if (right_wheel_joint_values > right_wheel_joint_values_threshold)
+  if (right_output > right_output_threshold)
     
-    stat.summaryf(diagnostic_msgs::DiagnosticStatus::ERROR, "%f.", right_wheel_joint_values);
+    stat.summaryf(diagnostic_msgs::DiagnosticStatus::ERROR, "Il valore e' fuori soglia! Soglia: %f.", right_output_threshold);
   else
     
-    stat.summaryf(diagnostic_msgs::DiagnosticStatus::OK, "Il valore rientra nella norma, right_wheel_joint_values: %f", right_wheel_joint_values);
+    stat.summaryf(diagnostic_msgs::DiagnosticStatus::OK, "Il valore rientra nella norma, output: %f", right_output);
 
   
   stat.add("Diagnostica right_wheel_joint", "Valore");
-  std::ostringstream right_wheel_joint_values_string;
-  right_wheel_joint_values_string << right_wheel_joint_values;
-  stat.addf("Right Wheel output", right_wheel_joint_values_string.str().c_str());
+  std::ostringstream right_output_string;
+  right_output_string << right_output;
+  stat.addf("Right Wheel output", right_output_string.str().c_str());
+}
+
+void RightJointPErrorDiagostic(diagnostic_updater::DiagnosticStatusWrapper &stat)
+{
+  if (right_p_error > right_p_error_threshold)
+    
+    stat.summaryf(diagnostic_msgs::DiagnosticStatus::ERROR, "Il valore e' fuori soglia! Soglia: %f.", right_p_error_threshold);
+  else
+    
+    stat.summaryf(diagnostic_msgs::DiagnosticStatus::OK, "Il valore rientra nella norma, p_error: %f", right_p_error);
+
+  
+  stat.add("Diagnostica right_wheel_joint", "Valore");
+  std::ostringstream right_p_error_string;
+  right_p_error_string << right_p_error;
+  stat.addf("Right Wheel p_error", right_p_error_string.str().c_str());
+}
+
+void RightJointIErrorDiagostic(diagnostic_updater::DiagnosticStatusWrapper &stat)
+{
+  if (right_i_error > right_i_error_threshold)
+    
+    stat.summaryf(diagnostic_msgs::DiagnosticStatus::ERROR, "Il valore e' fuori soglia! Soglia: %f.", right_i_error_threshold);
+  else
+    
+    stat.summaryf(diagnostic_msgs::DiagnosticStatus::OK, "Il valore rientra nella norma, i_error: %f", right_i_error);
+
+  
+  stat.add("Diagnostica right_wheel_joint", "Valore");
+  std::ostringstream right_i_error_string;
+  right_i_error_string << right_i_error;
+  stat.addf("Right Wheel i_error", right_i_error_string.str().c_str());
+}
+
+void RightJointDErrorDiagostic(diagnostic_updater::DiagnosticStatusWrapper &stat)
+{
+  if (right_d_error > right_d_error_threshold)
+    
+    stat.summaryf(diagnostic_msgs::DiagnosticStatus::ERROR, "Il valore e' fuori soglia! Soglia: %f.", right_d_error_threshold);
+  else
+    
+    stat.summaryf(diagnostic_msgs::DiagnosticStatus::OK, "Il valore rientra nella norma, d_error: %f", right_d_error);
+
+  
+  stat.add("Diagnostica right_wheel_joint", "Valore");
+  std::ostringstream right_d_error_string;
+  right_d_error_string << right_d_error;
+  stat.addf("Right Wheel d_error", right_d_error_string.str().c_str());
+}
+
+void RightJointErrorDiagostic(diagnostic_updater::DiagnosticStatusWrapper &stat)
+{
+  if (right_error > right_error_threshold)
+    
+    stat.summaryf(diagnostic_msgs::DiagnosticStatus::ERROR, "Il valore e' fuori soglia! Soglia: %f.", right_error_threshold);
+  else
+    
+    stat.summaryf(diagnostic_msgs::DiagnosticStatus::OK, "Il valore rientra nella norma, error: %f", right_error);
+
+  
+  stat.add("Diagnostica right_wheel_joint", "Valore");
+  std::ostringstream right_error_string;
+  right_error_string << right_error;
+  stat.addf("Right Wheel error", right_error_string.str().c_str());
+}
+
+void RightJointErrorDotDiagostic(diagnostic_updater::DiagnosticStatusWrapper &stat)
+{
+  if (right_error_dot > right_error_dot_threshold)
+    
+    stat.summaryf(diagnostic_msgs::DiagnosticStatus::ERROR, "Il valore e' fuori soglia! Soglia: %f.", right_error_dot_threshold);
+  else
+    
+    stat.summaryf(diagnostic_msgs::DiagnosticStatus::OK, "Il valore rientra nella norma, error_dot: %f", right_error_dot);
+
+  
+  stat.add("Diagnostica right_wheel_joint", "Valore");
+  std::ostringstream right_error_dot_string;
+  right_error_dot_string << right_error_dot;
+  stat.addf("Right Wheel error_dot", right_error_dot_string.str().c_str());
 }
 
 int main(int argc, char **argv)
@@ -35,13 +129,23 @@ int main(int argc, char **argv)
   ros::init(argc, argv, "right_wheel_joint_diagnostic");
   ros::NodeHandle nh;
   diagnostic_updater::Updater updater;
-  updater.setHardwareID("wheel_joint/right");
+  updater.setHardwareID("right_wheel_joint/state");
 
-  nh.getParam("/right_joint_wheel_params/right_joint_wheel_thresholds/right_wheel_joint_values_threshold", right_wheel_joint_values_threshold);
+  nh.getParam("/right_joint_wheel_params/right_joint_wheel_thresholds/output", right_output_threshold);
+  nh.getParam("/right_joint_wheel_params/right_joint_wheel_thresholds/p_error", right_p_error_threshold);
+  nh.getParam("/right_joint_wheel_params/right_joint_wheel_thresholds/i_error", right_i_error_threshold);
+  nh.getParam("/right_joint_wheel_params/right_joint_wheel_thresholds/d_error", right_d_error_threshold);
+  nh.getParam("/right_joint_wheel_params/right_joint_wheel_thresholds/error", right_error_threshold);
+  nh.getParam("/right_joint_wheel_params/right_joint_wheel_thresholds/error_dot", right_error_dot_threshold);
 
   ros::Subscriber sub = nh.subscribe("/gazebo_ros_control/pid_gains/right_wheel_joint/state", 1000, RightJointCallback);
 
-  updater.add("Diagnostica Joint right wheel: ", RightJointDiagostic);
+  updater.add("Diagnostica Output Joint Right Wheel: ", RightJointOutputDiagostic);
+  updater.add("Diagnostica P error Joint Right Wheel: ", RightJointPErrorDiagostic);
+  updater.add("Diagnostica I error Joint Right Wheel: ", RightJointIErrorDiagostic);
+  updater.add("Diagnostica D error Joint Right Wheel: ", RightJointDErrorDiagostic);
+  updater.add("Diagnostica error Joint Right Wheel: ", RightJointErrorDiagostic);
+  updater.add("Diagnostica error_dot Joint Right Wheel: ", RightJointErrorDotDiagostic);
 
   while (nh.ok())
   {
