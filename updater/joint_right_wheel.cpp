3a68f89107d81242275d883c036abc4852756540
nsilveri <nikis900@gmail.com>
2022-04-14 21:50:07 +0200

    Aggiunti commenti al codice

--- updater/joint_right_wheel.cpp
+++ updater/joint_right_wheel.cpp
@@ -1,9 +1,20 @@
+/*
+
+  il codice implementa il nodo per il controllo dei valori di joint_left_wheel e del PID
+  per il rilevamento a soglia per rilevare eventuali valori anomali.
+
+*/
 #include <diagnostic_updater/diagnostic_updater.h>
 #include <std_msgs/Bool.h>
 #include <diagnostic_updater/publisher.h>
 #include <control_msgs/PidState.h>
 #include <sstream>
 
+/*
+
+  Variabili globali per la memorizzazione dei valori attuali dei sensori e delle soglie.
+
+*/
 float right_output = 0.0;
 float right_output_threshold = 0.0;
 float right_p_error = 0.0;
@@ -17,7 +28,11 @@ float right_error_threshold = 0.0;
 float right_error_dot = 0.0;
 float right_error_dot_threshold = 0.0;
 
+/*
 
+  La RightJointStateCallback permette di salvare nelle variabili globali i valori attuali dei sensori.
+
+*/
 void RightJointCallback(const control_msgs::PidState::ConstPtr& msg)
 {
   right_output = msg->output;
@@ -28,6 +43,11 @@ void RightJointCallback(const control_msgs::PidState::ConstPtr& msg)
   right_error_dot = msg->error_dot;
 }
 
+/*
+  La funzione RightJointOutputDiagostic implementa il rilevamento a soglia per la variabile output creando il messaggio diagnostico
+  che viene pubblicato nel topic /diagnostics.
+  
+*/
 void RightJointOutputDiagostic(diagnostic_updater::DiagnosticStatusWrapper &stat)
 {
   if (right_output > right_output_threshold)
@@ -44,6 +64,10 @@ void RightJointOutputDiagostic(diagnostic_updater::DiagnosticStatusWrapper &stat
   stat.addf("Right Wheel output", right_output_string.str().c_str());
 }
 
+/*
+  la funzione RightJointPErrorDiagostic implementa il rilevamento a soglia per la variabile p_error creando il messaggio diagnostico
+  che viene pubblicato nel topic /diagnostics.
+*/
 void RightJointPErrorDiagostic(diagnostic_updater::DiagnosticStatusWrapper &stat)
 {
   if (right_p_error > right_p_error_threshold)
@@ -60,6 +84,10 @@ void RightJointPErrorDiagostic(diagnostic_updater::DiagnosticStatusWrapper &stat
   stat.addf("Right Wheel p_error", right_p_error_string.str().c_str());
 }
 
+/*
+  la funzione RightJointIErrorDiagostic implementa il rilevamento a soglia per la variabile i_error creando il messaggio diagnostico
+  che viene pubblicato nel topic /diagnostics.
+*/
 void RightJointIErrorDiagostic(diagnostic_updater::DiagnosticStatusWrapper &stat)
 {
   if (right_i_error > right_i_error_threshold)
@@ -76,6 +104,10 @@ void RightJointIErrorDiagostic(diagnostic_updater::DiagnosticStatusWrapper &stat
   stat.addf("Right Wheel i_error", right_i_error_string.str().c_str());
 }
 
+/*
+  la funzione RightJointDErrorDiagostic implementa il rilevamento a soglia per la variabile d_error creando il messaggio diagnostico
+  che viene pubblicato nel topic /diagnostics.
+*/
 void RightJointDErrorDiagostic(diagnostic_updater::DiagnosticStatusWrapper &stat)
 {
   if (right_d_error > right_d_error_threshold)
@@ -92,6 +124,10 @@ void RightJointDErrorDiagostic(diagnostic_updater::DiagnosticStatusWrapper &stat
   stat.addf("Right Wheel d_error", right_d_error_string.str().c_str());
 }
 
+/*
+  la funzione RightJointErrorDiagostic implementa il rilevamento a soglia per la variabile error creando il messaggio diagnostico
+  che viene pubblicato nel topic /diagnostics.
+*/
 void RightJointErrorDiagostic(diagnostic_updater::DiagnosticStatusWrapper &stat)
 {
   if (right_error > right_error_threshold)
@@ -108,6 +144,10 @@ void RightJointErrorDiagostic(diagnostic_updater::DiagnosticStatusWrapper &stat)
   stat.addf("Right Wheel error", right_error_string.str().c_str());
 }
 
+/*
+  la funzione RightJointErrorDotDiagostic implementa il rilevamento a soglia per la variabile error_dot creando il messaggio diagnostico
+  che viene pubblicato nel topic /diagnostics.
+*/
 void RightJointErrorDotDiagostic(diagnostic_updater::DiagnosticStatusWrapper &stat)
 {
   if (right_error_dot > right_error_dot_threshold)
@@ -130,7 +170,11 @@ int main(int argc, char **argv)
   ros::NodeHandle nh;
   diagnostic_updater::Updater updater;
   updater.setHardwareID("right_wheel_joint/state");
+  /*
 
+    Caricamento delle variabili globali con le soglie ottenute dai valori raccolti dal file YAML di configurazione.
+
+  */
   nh.getParam("/right_joint_wheel_params/right_joint_wheel_thresholds/output", right_output_threshold);
   nh.getParam("/right_joint_wheel_params/right_joint_wheel_thresholds/p_error", right_p_error_threshold);
   nh.getParam("/right_joint_wheel_params/right_joint_wheel_thresholds/i_error", right_i_error_threshold);
@@ -138,6 +182,13 @@ int main(int argc, char **argv)
   nh.getParam("/right_joint_wheel_params/right_joint_wheel_thresholds/error", right_error_threshold);
   nh.getParam("/right_joint_wheel_params/right_joint_wheel_thresholds/error_dot", right_error_dot_threshold);
 
+  /*
+
+    Il metodo subscribe permette di sottoscriversi al topic /right_wheel_joint/state per estrarne i messaggi.
+    Il metodo richiama le funzioni RightJointOutputDiagostic, RightJointPErrorDiagostic, RightJointIErrorDiagostic,
+      RightJointDErrorDiagostic, RightJointErrorDiagostic, RightJointErrorDotDiagostic.
+
+  */
   ros::Subscriber sub = nh.subscribe("/gazebo_ros_control/pid_gains/right_wheel_joint/state", 1000, RightJointCallback);
 
   updater.add("Diagnostica Output Joint Right Wheel: ", RightJointOutputDiagostic);
